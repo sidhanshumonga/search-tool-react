38ff4f25e0d02909bc9d9561a3f00e0184aa4d40
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2020-03-19 14:02:02 +0530

    updated code for photo grid with api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Row, Col } from 'react-bootstrap'
 import './App.css';
+import PhotosGrid from './components/Photos-grid'
+import { logo } from './logo.svg'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { value: '', photos: [] }
+  }
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+    if (e.target.value) {
+      axios.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=3b8da0e5c9ae44b2d9c8f009a21f8929&text=' + e.target.value + '&format=json&nojsoncallback=1')
+        .then(response => {
+          this.setState({ photos: [...response.data.photos.photo] })
+        });
+    }
+    else {
+      this.setState({ photos: [] });
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Row className="search-row">
+            <Col>
+              <h3 className="my-3">Search Photos</h3>
+              <Form>
+                <Form.Control className="search-input" placeholder="Search images..." value={this.state.value} onChange={this.handleChange} autoComplete="on" />
+              </Form>
+            </Col>
+          </Row>
+        </header>
+        <body className="App-body mt-3">
+          <PhotosGrid photos={this.state.photos}></PhotosGrid>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
