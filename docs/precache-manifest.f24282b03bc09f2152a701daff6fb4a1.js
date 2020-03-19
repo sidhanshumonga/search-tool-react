b4e4448bafd4c406962a18c1ec250ba0cb4e3cf4
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2020-03-20 00:50:53 +0530

    updated docs folder for gh-pages

---
+++ docs/precache-manifest.f24282b03bc09f2152a701daff6fb4a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add473dbaf0abcbf382e3e6bba4493d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e392f00386ac708c26a",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fc736e06e402060c6826",
+    "url": "./static/css/main.0447eb87.chunk.css"
+  },
+  {
+    "revision": "8e392f00386ac708c26a",
+    "url": "./static/js/2.8130b833.chunk.js"
+  },
+  {
+    "revision": "4ef169e897079a10287750a24d31d242",
+    "url": "./static/js/2.8130b833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc736e06e402060c6826",
+    "url": "./static/js/main.c91bb2c6.chunk.js"
+  },
+  {
+    "revision": "f8ded35a888d07b09d20",
+    "url": "./static/js/runtime-main.6dddef3b.js"
+  },
+  {
+    "revision": "4c14021095fd20edc900e2e9b91d318e",
+    "url": "./static/media/loading.4c140210.gif"
+  }
+]);
